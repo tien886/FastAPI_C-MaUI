7d587a7c11fa3527f7f3c2fb0b808167f5d238d0
TienTheKnightMare <tienvm886@gmail.com>
2025-10-04 09:28:22 +0700

    dependency injection

---
+++ Module01/Module01/DI/AppModule.cs
@@ -0,0 +1,15 @@
+﻿
+using Module01.ViewModels;
+using Module01.Views;
+
+namespace Module01.DI;
+
+public static class AppModule
+{
+    public static IServiceCollection RegisterDependency(this IServiceCollection services)
+    {
+        services.AddTransient<ManHinhChoView>();
+        services.AddTransient<ManHinhChoViewModel>();
+        return services;
+    }
+}
