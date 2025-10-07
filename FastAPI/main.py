59b070599632dad13a6449a967a7b60c3c1ca11a
TienTheKnightMare <tienvm886@gmail.com>
2025-10-07 12:58:31 +0700

    update capture image logic

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import json, os
+import base64
 app = FastAPI()
 # os.makedirs("JsonVault", exist_ok=True)
+os.makedirs("FastAPI", exist_ok=True)   #Ensure the image storage dir exist 
 class InputData(BaseModel):
     numbers: list[int]
     operators: str
@@ -47,5 +49,25 @@ async def execute_math(data: InputData):
         return OutputData(result=result)
     except ValueError as e:
         return {"error": str(e)}
-        
-            
+
+#
+class InputImage(BaseModel):
+    Base64stringFaceOfUser: str
+    UserName: str
+    UserId : str
+class OutputImage(BaseModel):
+    pass
+
+@app.post("/execute_image", response_model=OutputImage)
+async def execute_image(data: InputImage):
+    decodeddata = base64.b64decode(data.Base64stringFaceOfUser)
+    filepath = f"FastAPI/ImageFromC#/{data.UserId}.jpg"
+    with open(filepath, "wb") as f:
+        f.write(decodeddata)
+    if(filepath):
+        print("✅ Image saved successfully:", filepath)
+    else:
+        print("Fail to open file")
+    return OutputImage()
+      
+    
